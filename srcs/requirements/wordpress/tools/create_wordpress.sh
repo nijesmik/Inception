3c59a3d28d092c1e64e67d52d6d96c0479d6939d
Junsang Moon <mun.js@aol.com>
2024-06-13 15:31:27 +0900

    feat: update

--- srcs/requirements/wordpress/tools/create_wordpress.sh
+++ srcs/requirements/wordpress/tools/create_wordpress.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
 
-#check if wp-config.php exist
 if [ -f ./wp-config.php ]
 then
+	# wp-config 존재
 	echo "err: wp-config exists"
 else
+	# https://ko.wordpress.org/download/#download-install
 	wget http://wordpress.org/latest.tar.gz
 	tar xfz latest.tar.gz
 	mv wordpress/* .
 	rm -rf latest.tar.gz
 	rm -rf wordpress
 
+	# https://developer.wordpress.org/advanced-administration/wordpress/wp-config/
+	# https://github.com/WordPress/WordPress/blob/master/wp-config-sample.php
 	cp wp-config-sample.php wp-config.php
 	sed -i "s/username_here/$MYSQL_USER/g" wp-config.php
 	sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config.php
