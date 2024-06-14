001bfccbe69182ce8feb3b5a9dd2df4d93a69c1d
Junsang Mun <mun.js@aol.com>
2024-06-15 07:26:43 +0900

    feat: it works

--- Makefile
+++ Makefile
@@ -14,3 +14,7 @@ clean:
 	docker rm $$(docker ps -qa);
 	docker rmi -f $$(docker images -qa);
 	docker volume rm $$(docker volume ls -q);
+
+fclean: clean
+	rm -rf ./data/mysql/*
+	rm -rf ./data/wordpress/*
--- srcs/requirements/wordpress/tools/create_wordpress.sh
+++ srcs/requirements/wordpress/tools/create_wordpress.sh
@@ -14,11 +14,25 @@ else
 
 	# https://developer.wordpress.org/advanced-administration/wordpress/wp-config/
 	# https://github.com/WordPress/WordPress/blob/master/wp-config-sample.php
+	sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
+	sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
+	sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
+	sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
 	cp wp-config-sample.php wp-config.php
-	sed -i "s/username_here/$MYSQL_USER/g" wp-config.php
-	sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config.php
-	sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config.php
-	sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config.php
+
+	# https://nolboo.kim/blog/2016/05/16/ultimate-wordpress-development-environment-wp-cli/
+	curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+	chmod +x wp-cli.phar
+	mv wp-cli.phar /usr/local/bin/wp
+	wp core install	--url=$DOMAIN_NAME \
+					--title="Do you think this is fun?" \
+					--admin_user=$WP_ROOT_USERNAME \
+					--admin_password=$WP_ROOT_PASSWORD \
+					--admin_email="iam@junsang.dev" \
+					--allow-root
+	wp user create $WP_USERNAME junmoon@student.42seoul.kr \
+					--user_pass=$WP_PASSWORD \
+					--allow-root
 fi
 
 exec "$@"
