c454dfa56d28c56552b2f47f33c0f73d60062b07
Junsang Moon <junmoon@c1r1s4.42seoul.kr>
2024-06-17 16:30:28 +0900

    feat: revert

--- srcs/requirements/mariadb/script/sql_setup.sh
+++ srcs/requirements/mariadb/script/sql_setup.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 mysqld --user=mysql --datadir=/var/lib/mysql --bootstrap << EOF
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
 CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
@@ -7,6 +5,3 @@ CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
 GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
 FLUSH PRIVILEGES;
 EOF
-
-# Start the MariaDB server
-exec mysqld --user=mysql --datadir=/var/lib/mysql
