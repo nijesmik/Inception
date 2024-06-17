2837e4c959fdc6b70ce54d4a2b41ebcacc8ea9fc
Junsang Moon <junmoon@c1r1s4.42seoul.kr>
2024-06-17 14:50:25 +0900

    test: testing sql setup with env variables

--- srcs/requirements/mariadb/script/create_initial_sql.sh
+++
@@ -1,5 +0,0 @@
-echo "CREATE DATABASE IF NOT EXISTS wordpress;                                              -- create database named wordpress" >> initial.sql
-echo "CREATE USER IF NOT EXISTS '$MYSQL_DATABASE'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';      -- create user named junmoon with password" >> initial.sql
-echo "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';                       -- grant privileges on the wordpress* to the junmoon" >> initial.sql
-echo "FLUSH PRIVILEGES;                                                                     -- take effect of GRANT command" >> initial.sql
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';                   -- change root user password" >> initial.sql
