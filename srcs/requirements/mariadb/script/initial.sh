24c9fe74fef11d4ccb09ba9713bd241777fab0a0
Junsang Moon <junmoon@c1r1s4.42seoul.kr>
2024-06-17 19:50:48 +0900

    chroe: fix typo

--- srcs/requirements/mariadb/script/initial.sh
+++ srcs/requirements/mariadb/script/initial.sh
@@ -2,4 +2,4 @@ echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;" >> initial.sql
 echo "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> initial.sql
 echo "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';" >> initial.sql
 echo "FLUSH PRIVILEGES;" >> initial.sql
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';" >> initial.sqlMYSQL_PASSWORD
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';" >> initial.sql
