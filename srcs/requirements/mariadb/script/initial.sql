c2c1e8adb4488d6be26f4afa29c59b0411f30c0e
Junsang Moon <junmoon@c1r1s4.42seoul.kr>
2024-06-17 13:53:12 +0900

    feat: add comments in the sql file

--- srcs/requirements/mariadb/script/initial.sql
+++ srcs/requirements/mariadb/script/initial.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE IF NOT EXISTS wordpress;
-CREATE USER IF NOT EXISTS 'junmoon'@'%' IDENTIFIED BY '1q2w3e4r';
-GRANT ALL PRIVILEGES ON wordpress.* TO 'junmoon'@'%';
-FLUSH PRIVILEGES;
-ALTER USER 'root'@'localhost' IDENTIFIED BY '1q2w3e4r5t';
+CREATE DATABASE IF NOT EXISTS wordpress;                            -- create database named wordpress
+CREATE USER IF NOT EXISTS 'junmoon'@'%' IDENTIFIED BY '1q2w3e4r';   -- create user named junmoon with password
+GRANT ALL PRIVILEGES ON wordpress.* TO 'junmoon'@'%';               -- grant privileges on the wordpress* to the junmoon
+FLUSH PRIVILEGES;                                                   -- take effect of GRANT command
+ALTER USER 'root'@'localhost' IDENTIFIED BY '1q2w3e4r5t';           -- change root user password
