7f71f9159b051f7abe21d6964560ba010edb4dae
Junsang Moon <junmoon@c1r1s4.42seoul.kr>
2024-06-17 14:55:14 +0900

    feat: remove initial.sql

--- srcs/requirements/mariadb/script/initial.sql
+++
@@ -1,5 +0,0 @@
-CREATE DATABASE IF NOT EXISTS wordpress;                            -- create database named wordpress
-CREATE USER IF NOT EXISTS 'junmoon'@'%' IDENTIFIED BY '1q2w3e4r';   -- create user named junmoon with password
-GRANT ALL PRIVILEGES ON wordpress.* TO 'junmoon'@'%';               -- grant privileges on the wordpress* to the junmoon
-FLUSH PRIVILEGES;                                                   -- take effect of GRANT command
-ALTER USER 'root'@'localhost' IDENTIFIED BY '1q2w3e4r5t';           -- change root user password
