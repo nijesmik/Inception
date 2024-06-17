bfcf86c933e2e3d95c3216d2ddf6c276098035d8
Junsang Moon <junmoon@c1r1s4.42seoul.kr>
2024-06-17 19:51:18 +0900

    feat: testing: change path in inital.sh

--- srcs/requirements/mariadb/script/initial.sh
+++ srcs/requirements/mariadb/script/initial.sh
@@ -1,5 +1,5 @@
-echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;" >> initial.sql
-echo "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> initial.sql
-echo "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';" >> initial.sql
-echo "FLUSH PRIVILEGES;" >> initial.sql
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';" >> initial.sql
+echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;" >> /var/www/initial.sql
+echo "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> /var/www/initial.sql
+echo "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';" >> /var/www/initial.sql
+echo "FLUSH PRIVILEGES;" >> /var/www/initial.sql
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';" >> /var/www/initial.sql
