e354ca4d2ede9ab274db5ff365e7e8165e681a67
Junsang Moon <junmoon@c1r1s4.42seoul.kr>
2024-06-17 19:54:32 +0900

    feat: testing: update inital.sh

--- srcs/requirements/mariadb/script/initial.sh
+++ srcs/requirements/mariadb/script/initial.sh
@@ -1,4 +1,4 @@
-echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;" >> /var/www/initial.sql
+echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;" > /var/www/initial.sql
 echo "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> /var/www/initial.sql
 echo "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';" >> /var/www/initial.sql
 echo "FLUSH PRIVILEGES;" >> /var/www/initial.sql
