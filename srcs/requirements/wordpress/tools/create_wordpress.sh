fe611e57e553a1d3b6c17c17b5991e9a925c11ae
Junsang Moon <junmoon@c1r1s4.42seoul.kr>
2024-06-17 17:23:48 +0900

    feat: testing: add debug option

--- srcs/requirements/wordpress/tools/create_wordpress.sh
+++ srcs/requirements/wordpress/tools/create_wordpress.sh
@@ -18,6 +18,7 @@ else
 	sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
 	sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
 	sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
+	sed -i "s/define( 'WP_DEBUG', false );/define( 'WP_DEBUG', true );/g" wp-config-sample.php
 	cp wp-config-sample.php wp-config.php
 
 	# https://nolboo.kim/blog/2016/05/16/ultimate-wordpress-development-environment-wp-cli/
