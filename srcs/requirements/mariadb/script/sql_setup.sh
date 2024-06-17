ef2e8c1e363dd938bc5271ca51b75ef6e24e5364
Junsang Moon <junmoon@c1r1s4.42seoul.kr>
2024-06-17 17:03:34 +0900

    feat: testing

--- srcs/requirements/mariadb/script/sql_setup.sh
+++ srcs/requirements/mariadb/script/sql_setup.sh
@@ -1,5 +1,3 @@
-service mysql start
-
 mysqld --user=mysql --datadir=/var/lib/mysql --bootstrap << EOF
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
 CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
@@ -7,5 +5,3 @@ CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
 GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
 FLUSH PRIVILEGES;
 EOF
-
-exec mysqld --user=mysql --datadir=/var/lib/mysql
