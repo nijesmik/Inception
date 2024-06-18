5e0f833a1171585aa082b22656ff638841c65396
Junsang-Mun <mun.js@aol.com>
2024-06-18 16:38:30 +0900

    testing: CMD shell script

--- srcs/requirements/mariadb/script/initial.sh
+++ srcs/requirements/mariadb/script/initial.sh
@@ -2,10 +2,16 @@ if [ -f .env ]; then
     source .env
 fi
 
+service mysql start
+
 echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;" > /var/www/initial.sql
 echo "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> /var/www/initial.sql
 echo "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';" >> /var/www/initial.sql
 echo "FLUSH PRIVILEGES;" >> /var/www/initial.sql
 echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';" >> /var/www/initial.sql
 
-service mysql start && mysql -uroot -p < /var/www/initial.sql
+mysql < /var/www/initial.sql
+
+kill $(cat /var/run/mysqld/mysqld.pid)
+
+mysqld
