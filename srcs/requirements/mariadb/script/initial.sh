505bbd9168fa1cab7b749d744c0e691d298300cd
nijesmik <sejin1398@gmail.com>
2024-11-13 23:16:00 +0900

    👔 mariadb dockerfile 및 shell script 수정

--- srcs/requirements/mariadb/script/initial.sh
+++ srcs/requirements/mariadb/script/initial.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Start MySQL service
-service mysql start
+service mariadb start
 
 # Wait for MySQL to be ready
-until mysqladmin ping &>/dev/null; do
-  echo "Waiting for MySQL to be ready..."
+until mariadb-admin ping &>/dev/null; do
+  echo "Waiting for MariaDB to be ready..."
   sleep 2
 done
 
@@ -19,4 +19,4 @@ ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
 EOF
 
 # Execute the initial SQL script with the root password
-mysql -uroot -p"$MYSQL_ROOT_PASSWORD" < /var/www/initial.sql
+mariadb -uroot -p"$MYSQL_ROOT_PASSWORD" < /var/www/initial.sql
