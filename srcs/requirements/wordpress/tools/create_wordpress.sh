314c05d02fde3c756d9df2b4f96ad26f364c636b
nijesmik <sejin1398@gmail.com>
2024-11-14 01:36:27 +0900

    :necktie: wordpress database 설정 변경

--- srcs/requirements/wordpress/tools/create_wordpress.sh
+++ srcs/requirements/wordpress/tools/create_wordpress.sh
@@ -3,7 +3,7 @@
 if [ -f ./wp-config.php ]
 then
 	# wp-config 존재
-	echo "err: wp-config exists"
+	echo "wp-config exists"
 else
 	# https://ko.wordpress.org/download/#download-install
 	wget http://wordpress.org/latest.tar.gz
@@ -12,18 +12,17 @@ else
 	rm -rf latest.tar.gz
 	rm -rf wordpress
 
-	# https://developer.wordpress.org/advanced-administration/wordpress/wp-config/
-	# https://github.com/WordPress/WordPress/blob/master/wp-config-sample.php
-	sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
-	sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
-	sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
-	sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
-	cp wp-config-sample.php wp-config.php
-
 	# https://nolboo.kim/blog/2016/05/16/ultimate-wordpress-development-environment-wp-cli/
 	curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 	chmod +x wp-cli.phar
 	mv wp-cli.phar /usr/local/bin/wp
+
+	wp config create --dbname=$MYSQL_DATABASE \
+					 --dbuser=$MYSQL_USER \
+					 --dbpass=$MYSQL_PASSWORD \
+					 --dbhost=$MYSQL_HOSTNAME \
+					 --allow-root
+
 	wp core install	--url=$DOMAIN_NAME \
 					--title=$WP_TITLE \
 					--admin_user=$WP_ROOT_NAME \
